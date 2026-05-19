fead8bd4dda38464ccab91087c016372b80cac78
essanchristian-maker <essanchristian@gmail.com>
2026-05-19 22:43:33 +0000

    EXERCICE 3 END

---
+++ Week1/Day1/ExerciseXP/Exercice3.py
@@ -0,0 +1,11 @@
+#5 < 3                        False
+#3 == 3                       True 
+#3 == "3"                     False 
+#"3" > 3                      Erreur 
+#"Hello" == "hello"           False 
+
+print(5 < 3)
+print(3 == 3)
+print(3 == "3")
+print("3" > 3)
+print("Hello" == "hello")
